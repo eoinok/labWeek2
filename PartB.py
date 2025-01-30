5a2c755963266b1b17c617f7d263cc5c7808d295
Eoin O'Kennedy <Eoin.OKennedy@tudublin.ie>
2025-01-30 10:53:58 +0000

    part b

---
+++ PartB.py
@@ -0,0 +1,16 @@
+#Joe Bloggs
+#30/01/2025
+#A Pro
+
+def printSteps(a,b):
+    for i in range(a,b+1):
+        print("Step " + str(i))
+        
+print("Step 1")
+print("Step 2")
+print("Step 3")
+printSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
+
